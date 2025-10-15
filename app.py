d36c03b599f4374784782bf0fef22473bb889dec
Nicholas Wood <nick.wood@cdrg.org>
2025-10-15 09:38:41 -0500

    Adding constants file

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ from __future__ import annotations
 from dash import Dash
 from dash import html
 
-APP_TITLE = html.H1(
-    html.Strong(
-        className='hdg hdg_h1 mix-hdg_bold vr',
-        children='Divine Simplicity',
-    ),
-)
+from divine_simplicity.constants import APP_TITLE
 
 
 def main() -> int:
---
+++ divine_simplicity/constants.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from dash import html
+
+APP_TITLE = html.H1(
+    html.Strong(
+        className='hdg hdg_h1 mix-hdg_bold vr',
+        children='Divine Simplicity',
+    ),
+)
